d1649118869bdcae758d252d10da4b9337f1f311
Peter Belai <peter.belai@gmail.com>
2020-04-05 10:34:12 +0200

    coloring based on the dummy endpoint

--- ShinyApp/R/endpoints.R
+++ ShinyApp/R/endpoints.R
@@ -6,13 +6,12 @@ getNumberOfPeopleInArea <- function(areaName) {
   200
 }
 
-getAreas <- function() {
-  data.frame(
-    pointOfInterest = paste(c("area", "location", "position"), 1:15),
-    numberOfPeople = seq(11, 1000, 66),
-    area = c(50,100,150)
-  )
+getCurrentLocation <- function(lat, lng) {
+  data.frame(numberOfPeople = 720, area = 1000, name = "Meno")
+  data.frame(numberOfPeople = 2000, area = 1000, name = "Meno")
+  data.frame(numberOfPeople = 1, area = 1000, name = "Meno")
 }
+
 
 getAreaData <- function() {
   csvFile <- read.csv("dump.csv",encoding = "UTF-8")
